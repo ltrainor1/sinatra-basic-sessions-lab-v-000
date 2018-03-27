660bcc019610c4ee48227bd59937a552ffffa38b
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-27 14:41:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
 
   configure do
@@ -7,10 +7,10 @@ class App < Sinatra::Base
   set :session_secret, "secret"
 end
 
-get '/' do 
-end 
+get '/' do
+end
 
-get '/item' do 
-end 
+get '/item' do
+end
 
 end
